ce07a5d93e3b8849989f45e17202acccfdb04eaa
Winner <Winnerwr6@gmail.com>
2026-07-14 21:09:33 +0100

    actualização do login

---
+++ frontend/my-app/src/app/(auth)/auth/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "../../../features/auth/components/LoginForm";
+
+export default function Home() {
+  return (
+    <div className="">
+    <LoginForm />
+    </div>
+  );
+}
+
--- frontend/my-app/src/app/(auth)/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "../../features/auth/components/LoginForm";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    <LoginForm />
-    </div>
-  );
-}
