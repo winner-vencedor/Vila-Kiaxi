ddf64da6762e5d0fa97f94c29692212ea2e7e0fc
Winner <Winnerwr6@gmail.com>
2026-07-14 20:20:08 +0100

    tela de login

---
+++ frontend/my-app/src/app/(auth)/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "../features/auth/components/LoginForm";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <LoginForm />
+    </div>
+  );
+}
---
+++ frontend/my-app/src/app/features/auth/components/LoginForm.tsx
@@ -0,0 +1,7 @@
+export default function LoginForm() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <p>Login Form</p>
+    </div>
+  );
+}
