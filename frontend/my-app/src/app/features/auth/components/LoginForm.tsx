de262284aca78b029b837ed267ed69ad629745d0
Winner <Winnerwr6@gmail.com>
2026-07-14 20:27:35 +0100

    codando o login

--- frontend/my-app/src/app/features/auth/components/LoginForm.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoginForm() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <p>Login Form</p>
-    </div>
-  );
-}
---
+++ frontend/my-app/src/features/auth/components/LoginForm.tsx
@@ -0,0 +1,7 @@
+export default function LoginForm() {
+  return (
+    <div className="">
+      <p>Login Form</p>
+    </div>
+  );
+}
