d253297b482e0a142f2c7e385ac703ada5eafa6e
Winner <winnerwr6@gmail.com>
2026-09-03 21:11:09 +0100

    refactor:adaptando o bottom-sheet para translatey=0

--- frontend/my-app/src/shared/components/custom/Bottom-Sheet/bottom-sheet.tsx
+++ frontend/my-app/src/shared/components/custom/Bottom-Sheet/bottom-sheet.tsx
@@ -1,116 +1,52 @@
 'use client';
-import { useState, useEffect, useRef } from 'react';
-import { BottomSheetHandle } from '@/shared/components/custom/Bottom-Sheet/handle';
-import {
-  CLOSE_THRESHOLD,
-  SNAP_POINTS,
-} from '@/shared/components/custom/Bottom-Sheet/constants';
+import { useState } from 'react';
 
-type BottomSheetProps = {
+interface BottomSheetProps {
   open: boolean;
   onClose: () => void;
   children: React.ReactNode;
-};
+}
 
-export function BottomSheet({ open, onClose, children }: BottomSheetProps) {
-  const startY = useRef(0);
-  const startTranslateY = useRef(SNAP_POINTS.HALF);
-  const [translateY, setTranslateY] = useState(SNAP_POINTS.HALF);
-  const [dragging, setDragging] = useState(false);
-
-  function handlePointerDown(event: React.PointerEvent<HTMLDivElement>) {
-    setDragging(true);
-
-    startY.current = event.clientY;
-    startTranslateY.current = translateY;
-
-    setDragging(true);
-  }
-
-  useEffect(() => {
-    if (!open || !dragging) return;
-
-    function onPointerMove(event: PointerEvent) {
-      const distance = event.clientY - startY.current;
-      const newPosition = startTranslateY.current + distance;
-
-      const limitedPosition = Math.max(
-        SNAP_POINTS.EXPANDED,
-        Math.min(newPosition, CLOSE_THRESHOLD)
-      );
-      setTranslateY(limitedPosition);
-    }
-
-    function onPointerUp() {
-      setDragging(false);
-
-      if (translateY >= CLOSE_THRESHOLD) {
-        setTranslateY(SNAP_POINTS.HALF);
-        onClose();
-        return;
-      }
-      if (translateY < 150) {
-        setTranslateY(SNAP_POINTS.EXPANDED);
-
-        return;
-      }
-
-      if (translateY < 430) {
-        setTranslateY(SNAP_POINTS.HALF);
-
-        return;
-      }
-
-      setTranslateY(SNAP_POINTS.COLLAPSED);
-    }
-
-    window.addEventListener('pointermove', onPointerMove);
-    window.addEventListener('pointerup', onPointerUp);
-
-    return () => {
-      window.removeEventListener('pointermove', onPointerMove);
-      window.removeEventListener('pointerup', onPointerUp);
-    };
-  }, [open, dragging, translateY, onClose]);
-
-  useEffect(() => {
-    function handleEsc(event: KeyboardEvent) {
-      if (event.key === 'Escape') {
-        onClose();
-      }
-    }
-
-    window.addEventListener('keydown', handleEsc);
-
-    return () => {
-      window.removeEventListener('keydown', handleEsc);
-    };
-  }, [onClose]);
+export function BottomSheet({ open, children }: BottomSheetProps) {
+  const [translateY] = useState(10);
+  const [dragging] = useState(false);
 
   if (!open) return null;
+
   return (
     <>
       <div
-        className={`fixed inset-0 bg-black/50 md:hidden transition-opacity duration-300
+        className={`fixed inset-0 z-99 bottom-0 left-0   bg-black/50 md:hidden transition-opacity duration-300 overflow-hidden
               ${
                 open
                   ? 'opacity-100 pointer-events-auto'
                   : 'opacity-0 pointer-events-none'
               }
             `}
-        onClick={onClose}
-      />
-      <div
-        style={{
-          transform: `translateY(${translateY}px)`,
-        }}
-        className={`fixed bottom-0 left-0 w-full rounded-t-3xl bg-white p-3 shadow-2xl md:hidden touch-none select-none
-            ${dragging ? '' : 'transition-transform duration-300 ease-out'}`}
       >
-        <div className="flex justify-center pb-0">
-          <BottomSheetHandle onPointerDown={handlePointerDown} />
+        <div
+          style={{
+            transform: `translateY(${translateY}px)`,
+          }}
+          className={` fixed
+      inset-x-0
+      bottom-0
+      h-dvh
+      max-h-dvh
+      rounded-t-3xl
+      bg-gray-50
+      p-3
+      flex
+      flex-col
+            ${dragging ? '' : 'transition-transform duration-300 ease-out'}`}
+        >
+          <div className="flex justify-center items-center">
+            <div className="h-1 w-12 rounded-full bg-gray-500" />
+          </div>
+          <div className="flex-1 min-h-0 overflow-y-auto overscroll-contain overscroll-y-auto">
+            <div className="flex flex-col gap-3 pb-8 ">{children}</div>
+          </div>
         </div>
-        {children}
       </div>
     </>
   );
--- frontend/my-app/src/shared/components/custom/Bottom-Sheet/constants.ts
+++
@@ -1,7 +0,0 @@
-export const SNAP_POINTS = {
-  EXPANDED: 0,
-  HALF: 300,
-  COLLAPSED: 550,
-};
-
-export const CLOSE_THRESHOLD = 400;
